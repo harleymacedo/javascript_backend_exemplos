5a051d16e3480d2ad71d954ea4fc92445e43a5d6
Harley Macedo <harley.ip@gmail.com>
2020-06-26 16:22:14 -0300

    Resposta aprimorada

--- responses/app.js
+++ responses/app.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 
 //Resposta html
-app.get('/html')
-
+app.get('/html', function(req, res) { 
+    res.status(200)
+    res.type('html')
+    res.end('<h2>Resposta em html</h2>')
+})
 
 //Resposta json
-app.get('/json')
+app.get('/json', function(req, res) {
+    res.status(201)
+    res.type('json')
+    res.json({"nome": "Harley", "curso": "Sistemas de informação"})
+})
+
+app.listen(3000, function() {
+    console.log('App rodando na porta 3000!')
+})
